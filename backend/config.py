f3b82e7c35305274f3b6ecf4c26a69d1ffd94a52
Bryan <bryanlenin.angulo250@comunidadunir.net>
2025-01-03 16:58:56 -0500

    Carga archivos docker para uso Bryan

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user_generico:UserGenerico@localhost:5432/DB_PRUEBAS_1?client_encoding=utf8' #Jose
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user_generico:UserGenerico@localhost:5432/db_pruebas_1?client_encoding=utf8' #Jose
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "secret_key"
     CORS_HEADERS = "Content-Type"
